22c6b1b6f7e0b7b09a558c58198dcef0e8bd443f
Trex3067 <amitsahu.ltp@gmail.com>
2020-03-27 20:44:23 +0530

    Upload Website

---
+++ jscript6.js
@@ -0,0 +1,15 @@
+var color1=document.getElementsByName("color1")[0];
+var color2=document.getElementsByName("color2")[0];
+var text=document.getElementById("dytext");
+var bodybg=document.body.style;
+function gradientChange(){
+    bodybg.background="-webkit-linear-gradient(left,"+(color1.value)+","+(color2.value)+")";
+}
+function colorChange(){
+    text.innerText="";
+    text.appendChild(document.createTextNode(bodybg.background));
+    gradientChange();
+};
+colorChange();
+color1.addEventListener("input",colorChange);
+color2.addEventListener("input",colorChange);
